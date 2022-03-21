2ed22416a3b8e39cf2860dc4ac2469434bf4a922
Nathan Boisvert <nathan.boisvert@mymail.champlain.edu>
2022-03-21 13:43:17 -0400

    Initial Commit

---
+++ Assets/Scripts/InfluenceMap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+Informational Sources:
+http://www.gameaipro.com/GameAIPro2/GameAIPro2_Chapter29_Escaping_the_Grid_Infinite-Resolution_Influence_Mapping.pdf
+https://www.gameaipro.com/GameAIPro2/GameAIPro2_Chapter30_Modular_Tactical_Influence_Maps.pdf
+http://www.gameaipro.com/GameAIPro2/GameAIPro2_Chapter31_Spatial_Reasoning_for_Strategic_Decision_Making.pdf
+https://github.com/AliKarimi74/InfluenceMap/tree/master/Assets/Scripts
+https://forum.unity.com/threads/ai-influence-maps.145368/
+https://darkhorsegames.net/influence-maps-unity/
+https://www.gamedev.net/tutorials/programming/artificial-intelligence/the-core-mechanics-of-influence-mapping-r2799/
+https://vimeo.com/23913640
+*/
+
+public class InfluenceMap : MonoBehaviour
+{
+    public float [,] influences;
+}
